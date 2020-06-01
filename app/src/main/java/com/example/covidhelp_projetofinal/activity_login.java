066091341a4d135b3f136dfa6da56ea0b4f8e43c
rafaelcosta <rafaelcosta.10@live.com>
2020-06-01 11:16:41 +0100

    Criacao da atividade LogIn

---
+++ app/src/main/java/com/example/covidhelp_projetofinal/activity_login.java
@@ -0,0 +1,14 @@
+package com.example.covidhelp_projetofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
